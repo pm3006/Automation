af20029815faa86fd3193394b007dd6e44d782e2
pm3006 <pablomorab30@gmail.com>
2026-01-02 09:50:01 -0600

    login playwright framework no library

---
+++ Clase 4/index.js
@@ -0,0 +1,22 @@
+//array
+const carros =['hyundai','suzuki' ]
+console.log(carros)
+//para agregar al array luego de ser creado
+carros.push('Nissan')
+console.log(carros)
+
+const carroobjeto = {
+
+marca: 'suzuki',
+modelo: 'sidekick'
+}
+console.log(carroobjeto)
+//para agregar al objeto luego de ser creado
+carroobjeto.motor=1600
+
+console.log(carroobjeto)
+
+delete carroobjeto.motor
+
+console.log(carroobjeto)
+
